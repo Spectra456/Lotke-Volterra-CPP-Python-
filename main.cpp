2d8b89b6d7c1036a4cd38ac357c695739de5d64e
Spectra <spectranoid123@gmail.com>
2018-04-15 23:52:36 +0300

    Lotke-Volterra model C++ and Python

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// первый вариант
+//#define X_init 300
+//#define Y_init 150
+
+// второй вариант
+#define X_init 150
+#define Y_init 150
+
+#define H 0.5
+
+#define Alpha 2
+#define Beta 0.01
+#define Gamma 1
+#define Delta 0.01
+
+#define T_start 0.0
+
+#define T_finish 10
+
+
+double fx(double x, double y) { return (Alpha * x - Beta * x * y); }
+
+
+double fy(double x, double y) { return (-Gamma * y + Delta * x * y); }
+
+
+void runge_kutta(double *x, double *y, double h, double (*fx)(double, double),
+                 double (*fy)(double, double)) {
+    double dx1, dx2, dx3, dx4, dy1, dy2, dy3, dy4;
+
+    dx1 = fx(*x, *y);
+    dy1 = fy(*x, *y);
+    dx2 = fx(*x + (h / 2.0) * dx1, *y + (h / 2.0) * dy1);
+    dy2 = fy(*x + (h / 2.0) * dx1, *y + (h / 2.0) * dy1);
+    dx3 = fx(*x + (h / 2.0) * dx2, *y + (h / 2.0) * dy2);
+    dy3 = fy(*x + (h / 2.0) * dx2, *y + (h / 2.0) * dy2);
+    dx4 = fx(*x + h * dx3, *y + h * dy3);
+    dy4 = fy(*x + h * dx3, *y + h * dy3);
+
+    *x += h * (dx1 + 2.0 * dx2 + 2.0 * dx3 + dx4) / 6.0;
+    *y += h * (dy1 + 2.0 * dy2 + 2.0 * dy3 + dy4) / 6.0;
+}
+
+int main() {
+    double xm[10000],ym[10000];
+    double x, y;
+    double i = T_start;
+
+    x = X_init;
+    y = Y_init;
+
+    ofstream foutx("outx.txt");
+    ofstream fouty("outy.txt");
+
+    while (i <= T_finish){
+
+        foutx << x<<"," << "\n";
+        fouty << y <<","<< "\n";
+        runge_kutta(&x, &y, H, fx, fy);
+        i+=H;
+
+    }
+    foutx.close();
+    fouty.close();
+    return 0;
+}
